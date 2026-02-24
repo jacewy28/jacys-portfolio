4bfdd7bd3c168c111418482032e4504c5f3b1af8
Jacy Fu <jfu38@uic.edu>
2026-02-23 18:09:01 -0600

    moved about, contact, and projects pages to root directory and added header/div/etc

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,14 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-row gap-4">
+    <><div className="flex flex-row gap-4">
       <Link href="/about">About</Link>
       <Link href="/contact">Contact</Link>
       <Link href="/projects">Projects</Link>
-    </div>
+    </div><div className="flex flex-col gap-4 mt-4">
+        <h1>Hello</h1>
+        <p>This is a paragraph.</p>
+        <img src="/file.svg" alt="logo" width={30}></img>
+      </div></>
   );
 }
